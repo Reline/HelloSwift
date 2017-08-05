300c5d2163bdd8ed2abd67de5c519b4378393736
Reline <nathanielreline@gmail.com>
2017-08-04 22:16:48 -0400

    Print the sum of numbers between 1 and n

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,17 @@
-print("What's your name?")
-let response = readLine() ?? ""
-if response == "Alice" || response == "Bob" {
-	print("Hello \(response)!")
-} else {
-	print("Hello!")
+func ask() {
+  let response = readLine() ?? ""
+  guard let int = Int(response) else {
+    print("That was not a number, try again")
+    ask()
+    return
+  }
+
+  var result = 0
+  for i in 0...int {
+    result += i
+  }
+  print("The sum of every number from 0 to \(int) is \(result)")
 }
+
+print("Give me a number")
+ask()
